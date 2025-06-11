9c148bd1b1516167e3cd52e7649c46b31b5660b4
Siriwut <67328603+Younive@users.noreply.github.com>
2025-06-11 16:19:08 +0700

    add config.py to app directory

---
+++ app/config.py
@@ -0,0 +1,22 @@
+import os
+from dotenv import load_dotenv
+
+# Load environment variables from .env file
+load_dotenv(dotenv_path=os.path.join(os.path.dirname(__file__), '..', '.env'))
+
+BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+KNOWLEDGE_BASE_DIR = os.path.join(BASE_DIR, "knowledge_base")
+VECTOR_DB_DIR = os.path.join(BASE_DIR, "vector_db")
+
+# LLM & Embeddings
+GEMINI_API_KEY = os.getenv('GEMINI_API_KEY')
+LLM_MODEL_NAME = 'models/gemini-2.0-flash'
+BASE_URL = 'https://generativelanguage.googleapis.com/v1beta'
+EMBEDDING_MODEL_NAME = 'sentence-transformers/all-MiniLM-L6-v2'
+
+# Text Splitting
+CHUNK_SIZE = 1000
+CHUNK_OVERLAP = 200
+
+# Retriever
+SEARCH_K = 5
